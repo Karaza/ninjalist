fd097ba0fe4c886706893c3fd3abe4e77e1e89b7
Sophie Kowalski <skowalski57@gmail.com>
2021-02-06 16:48:08 +0100

    adding styles

--- pages/index.js
+++ pages/index.js
@@ -5,33 +5,21 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
+        dolores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Explicabo aperiam doloribus excepturi provident. Numquam, expedita
+        voluptatum a veritatis eligendi sint?
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores.
+        dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+        quae assumenda alias beatae odit explicabo omnis sit nesciunt aspernatur
+        minus!
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
