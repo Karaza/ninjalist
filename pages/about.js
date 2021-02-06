2015a23771b034fd64f355d83a821588e8c0957d
Sophie Kowalski <skowalski57@gmail.com>
2021-02-06 11:25:10 +0100

    Basic setup with first pages + Navbar and Footer basic components

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+        ea.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+        ea.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+        ea.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+        ea.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+        ea.
+      </p>
+    </div>
+  );
+};
+
+export default About;
