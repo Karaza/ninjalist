51d7a146b778ad2b05d15cdb41c0611bbdd2c289
Sophie Kowalski <skowalski57@gmail.com>
2021-02-07 17:52:21 +0100

    add metas

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,36 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
-        ea.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
-        ea.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
-        ea.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
-        ea.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
-        ea.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+          ea.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+          ea.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+          ea.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+          ea.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
+          ea.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,29 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Explicabo aperiam doloribus excepturi provident. Numquam, expedita
-        voluptatum a veritatis eligendi sint?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-        quae assumenda alias beatae odit explicabo omnis sit nesciunt aspernatur
-        minus!
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+          dolores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Explicabo aperiam doloribus excepturi provident. Numquam, expedita
+          voluptatum a veritatis eligendi sint?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+          dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+          quae assumenda alias beatae odit explicabo omnis sit nesciunt
+          aspernatur minus!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
